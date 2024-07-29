d3297c642fc3bfbde64c0a939137ad32790721cf
ajinkya sontakke <ajinkyasontakke07@gmail.com>
2024-07-29 23:00:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Legal from "./Pages/Legal";
+import NotFound from "./Pages/NotFound";
+import Appointment from "./Pages/Appointment";
+import About from "./Pages/About";
+import Reviews from "./Pages/Reviews";
+import Doctors from "./Pages/Doctors";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/legal" element={<Legal />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/doctors" element={<Doctors />} />
+        </Routes>
+        <Footer/>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../Components/Hero";
+import Info from "../Components/Info";
+
+
+function Home() {
+  return (
+    <div className="home-section">
+      <Hero />
+      <Info />
+    </div>
+  );
+}
+
+export default Home;
